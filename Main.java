3d1da17063a6921a7629995adc168b8db21c588e
Sean Leblanc <sean@Seans-MacBook-Air.local>
2026-07-05 20:13:44 -0700

    CarbonFootprint

--- Main.java
+++ Main.java
@@ -1,24 +1,27 @@
-// PARTNER NAME: 
-// PARTNER NAME: 
-// CS111 SECTION #: 
-// DATE: 
- 
-public class Main
-{
-	public static void main(String[] args)
-	{
-		//DECLARATION + INITIALIZATION SECTION
-		
-		//INPUT SECTION
-		// Still no input just yet...
-		
-		
-		//CALCULATION SECTION
-   
-    
-		//OUTPUT SECTION
-		
-	}
-	/* your methods here */
+// PARTNER NAME: Sean Leblanc
+// PARTNER NAME: Sean Leblanc
+// CS111 SECTION #: 09
+// DATE: 10/06/2026
 
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int vehicle = calcVehicleEmissions(25.0, 1200.0);
+        int gas = calcNaturalGasEmissions(50.0, 11.7);
+
+        System.out.println("Vehicle emissions: " + vehicle);
+        System.out.println("Natural gas emissions: " + gas);
+    }
+
+    public static int calcVehicleEmissions(double mpg, double avgMilesDriven) {
+        double emissions = (avgMilesDriven / mpg) * 19.6;
+        return (int) Math.round(emissions);
+    }
+
+    public static int calcNaturalGasEmissions(double natGasConsumption, double natGasEF) {
+        double emissions = natGasConsumption * natGasEF;
+        return (int) Math.round(emissions);
+    }
 }
